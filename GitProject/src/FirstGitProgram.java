e18b076e88f0caf10353185a356ae73787012cce
mahesh <mkalugotla@gmail.com>
2020-02-08 10:04:17 +0000

    first commit

---
+++ GitProject/src/FirstGitProgram.java
@@ -0,0 +1,15 @@
+
+public class FirstGitProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry = "yes";
+		if (hungry == "yes")
+		{
+			System.out.println("Eat Some Food");
+		}
+		else
+			System.out.println("Do Some Work");
+	}
+
+}
